cde1d015a473007449a565e102663ecf7052b889
Cam Anderson <camerona2468@gmail.com>
2023-01-31 23:00:57 -0500

    First day of work. 31/01/2023

--- todo_list/src/App.tsx
+++ todo_list/src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "./index.css";
+import Task from "./components/Task";
 
 const App = () => {
     return (
-        <div className="text-red-600" >Hello World</div>
+        <div className='w-1/2 mx-auto min-h-screen'>
+            <div className='flex justify-start py-2'>
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Add</button>
+            </div>
+            <div className='h-96'>
+                <div className='min-h-full bg-red-500 p-4 flex flex-col space-y-4'>
+                    <Task/>
+                </div>
+            </div>
+        </div> 
     );
 }
 
